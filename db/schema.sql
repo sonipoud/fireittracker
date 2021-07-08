30bf95616e8c3d956dc3c49084d1fef658371601
Sonika <sonikapoudyal5@gmail.com>
2021-07-08 13:59:34 -0400

    seeds.sql file updated

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,14 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL, 
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER 
 )
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
---
+++ db/seeds.sql
@@ -0,0 +1,36 @@
+INSERT INTO department (name)
+VALUES
+('Legal'),
+('Engineering'),
+('Sales'),
+('Finance')
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Legal team Lead', 95000, 1),
+('Lawyer', 170000, 1)
+('Lead Engineer', 120000, 2),
+('Software Engineer', 150000, 2),
+('Sales Lead', 30000, 3),
+('Salesperson', 25000, 3),
+('Accountant', 100000, 4),
+
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('James', 'Fraser',),
+  ('Jack', 'London',),
+  ('Robert', 'Bruce',),
+  ('Peter', 'Greenaway',),
+  ('Derek', 'Jarman', ),
+  ('Paolo', 'Pasolini',),
+  ('Heathcote', 'Williams',),
+  ('Sandy', 'Powell', ),
+  ('Emil', 'Zola',),
+  ('Sissy', 'Coalpits',),
+  ('Antoinette', 'Capet',),
+  ('Samuel', 'Delany',),
+  ('Tony', 'Duvert', ),
+  ('Dennis', 'Cooper',),
+  ('Monica', 'Bellucci',),
